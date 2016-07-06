d17775187626009bc5c62b262dfe943359f4f6a1
LXZ_svn_sync <lixizhi@yeah.net>
2016-07-06 11:45:39 +0800

    svn_sync

--- script/ide/System/Database/TableDatabase.lua
+++ script/ide/System/Database/TableDatabase.lua
@@ -13,17 +13,19 @@ Following is tested on my Intel-i7-3GHZ CPU with HDD. See `test` folder for deta
 ### Run With Conservative Mode
 Following is averaged value from 100000+ operations in a single thread
 
-* Random non-index insert: 43478 inserts/second
+* Random non-index insert: `43478 inserts/second`
    * Async API tested with default configuration with 1 million records on a single thread.
-* Round trip latency call: 20000 inserts/s (sync mode),  11ms or 85 query/s (async mode due to NPL time slice)
+* Round trip latency call: 
+   * Blocking API: `20000 query/s`
+   * Non-blocking API: `11ms` or `85 query/s` (due to NPL time slice)
    * i.e. Round strip means start next operation after previous one is returned. This is latency test.
-* Random indexed inserts: 17953 query/s
+* Random indexed inserts: `17953 query/s`
    * i.e. start next operation immediately, without waiting for the first one to return.
-* Random select with auto-index: 18761 query/s
+* Random select with auto-index: `18761 query/s`
    * i.e. same as above, but with findOne operation. 
-* Randomly mixing CRUD operations: 965-7518 query/s
+* Randomly mixing CRUD operations: `965-7518` query/s
    * i.e. same as above, but randomly calling Create/Read/Update/Delete (CRUD) on the same auto-indexed table.
-   * Mixing read/write can be very slow when database much bigger than cache size (default 2MB). 
+   * Mixing read/write can be slow when database grows bigger. e.g. you can get `18000 CRUD/s` for just 10000 records. 
 
 ### Run With Aggressive Mode
 One can also use in-memory journal file or ignore OS disk write feedback to further increase DB throughput by 30-100% percent.
