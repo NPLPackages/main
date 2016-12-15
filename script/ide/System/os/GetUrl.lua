f04274ddcb63dec0913d2d9735d6a369db8d1567
LXZ_svn_sync <lixizhi@yeah.net>
2016-12-15 18:06:29 +0800

    svn_sync

--- script/ide/System/os/GetUrl.lua
+++ script/ide/System/os/GetUrl.lua
@@ -14,7 +14,7 @@ System.os.GetUrl("https://github.com/LiXizhi/HourOfCode/archive/master.zip", ech
 System.os.GetUrl("https://github.com/LiXizhi/HourOfCode/archive/master.zip", function(err, msg, data)  echo(msg) end, "-I");
 -- send form KV pairs with http post
 System.os.GetUrl({url = "http://localhost:8099/ajax/console?action=getparams", form = {key="value",} }, function(err, msg, data)		echo(data)	end);
--- send multi-part binary forms with http post
+-- send multi-part binary file upload with http post
 System.os.GetUrl({url = "http://localhost:8099/ajax/console?action=printrequest", form = {name = {file="dummy.html",	data="<html><bold>bold</bold></html>", type="text/html"}, } }, function(err, msg, data)		echo(data)	end);
 -- To send any binary data, one can use 
 System.os.GetUrl({url = "http://localhost:8099/ajax/console?action=printrequest", headers={["content-type"]="application/json"}, postfields='{"key":"value"}' }, function(err, msg, data)		echo(data)	end);
