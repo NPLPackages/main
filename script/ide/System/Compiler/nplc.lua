2f55c1390d57955df97e9e84da2b7ee1cd0ca304
Zhiyuan <zyguo1230@126.com>
2017-01-05 18:21:15 +0800

    up to date

--- script/ide/System/Compiler/nplc.lua
+++ script/ide/System/Compiler/nplc.lua
@@ -6,7 +6,6 @@
 -- Date: 2016-12-13
 ----------------------------------------------------------------------
 
-NPL.load("(gl)script/ide/System/Compiler/nplp_def.lua");
 NPL.load("(gl)script/ide/System/Compiler/nplp.lua");
 NPL.load("(gl)script/ide/System/Compiler/nplgen.lua");
 
@@ -18,31 +17,8 @@ function nplc.compile(src_filename, dst_filename)
 	local src_file = assert(io.open (src_filename, 'r'))
 	local src = src_file:read '*a'; src_file:close()
 	--src = src:gsub('^#[^\r\n]*', '') 
-	--local nplpInt = nplp:new()
 	local ast = nplp:src_to_ast(src)
-
-	--table.print(ast, 80, "nohash")
 	local compiled_src= nplgen.ast_to_str(ast)
-	--print(compiled_src)
-	local dst_file = assert(io.open (dst_filename, 'w'))  -- debug only
-	dst_file:write(compiled_src)
-	dst_file:close()
-end
-
-function nplc.compile_Debug(src_filename, src_filename2, dst_filename)	-- debug only
-	local src_file = assert(io.open (src_filename, 'r'))
-	local src1 = src_file:read '*a'; src_file:close()
-
-	src_file = assert(io.open (src_filename2, 'r'))
-	local src2 = src_file:read '*a'; src_file:close()
-	--src = src:gsub('^#[^\r\n]*', '') 
-	local nplpInt = nplp:new()
-	local nplpInt2 = nplp:new()
-	local ast = nplpInt:src_to_ast(src1)
-	local ast2 = nplpInt:src_to_ast(src2)
-	--table.print(ast, 80, "nohash")
-	local compiled_src= nplgen.ast_to_str(ast2)
-	--print(compiled_src)
 	local dst_file = assert(io.open (dst_filename, 'w'))  -- debug only
 	dst_file:write(compiled_src)
 	dst_file:close()
@@ -55,7 +31,6 @@ function nplc.loadstring(string, filename, nplp_obj)
 	else
 		ast = nplp:src_to_ast(string)
 	end
-	--local ast = nplp.src_to_ast(string)
 	local compiled_src= nplgen.ast_to_str(ast)
 	return loadstring(compiled_src, filename)
 end
