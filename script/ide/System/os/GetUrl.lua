2b562246eacc12fff250fb08672bccdb64f181fc
LXZ_svn_sync <lixizhi@yeah.net>
2017-01-08 22:14:39 +0800

    svn_sync

--- script/ide/System/os/GetUrl.lua
+++ script/ide/System/os/GetUrl.lua
@@ -132,13 +132,15 @@ function Request:SetResponse(msg)
 	if(msg and msg.data) then
 		if(type(msg.header) == "string") then
 			local input_type_lower = msg.header:lower():match("content%-type:%s*([^\r\n]+)");
-			if(input_type_lower:find("application/json", 1, true)) then
-				if(type(msg.data) == "string") then
-					msg.data = commonlib.Json.Decode(msg.data) or msg.data;
-				end
-			elseif(input_type_lower:find("x-www-form-urlencoded", 1, true)) then
-				-- TODO: 
-			end 
+			if(input_type_lower) then
+				if(input_type_lower:find("application/json", 1, true)) then
+					if(type(msg.data) == "string") then
+						msg.data = commonlib.Json.Decode(msg.data) or msg.data;
+					end
+				elseif(input_type_lower:find("x-www-form-urlencoded", 1, true)) then
+					-- TODO: 
+				end 
+			end
 		end
 	end
 end
