cd79c4e2754cfce16412cc091392e2271e89786b
LXZ_svn_sync <lixizhi@yeah.net>
2017-01-22 18:06:02 +0800

    svn_sync

--- script/ide/System/Compiler/lib/mlp_meta.lua
+++ script/ide/System/Compiler/lib/mlp_meta.lua
@@ -39,13 +39,13 @@ end
 -- the only key being lifted in this version is ["tag"]
 --------------------------------------------------------------------------------
 function mlp.quote (t)
-   --print("QUOTING:", table.tostring(t, 60))
+   --print("QUOTING:", util.table_tostring(t, 60))
    local cases = { }
    function cases.table (t)
       local mt = { tag = "Table" }
       --table.insert (mt, { tag = "Pair", quote "quote", { tag = "True" } })
       if t.tag == "Splice" then
-		 printf("IN_QUOTE:\n%s", table.tostring(t, "nohash", 60))
+		 printf("IN_QUOTE:\n%s", util.table_tostring(t, "nohash", 60))
          assert (#t==1, "Invalid splice")
          local sp = t[1]
          return sp
@@ -84,14 +84,14 @@ function mlp.splice_content (lx)
    end
    local ast = mlp[parser_name](lx)
    if mlp.in_a_quote then
-      printf("SPLICE_IN_QUOTE:\n%s", table.tostring(ast, "nohash", 60))
+      printf("SPLICE_IN_QUOTE:\n%s", util.table_tostring(ast, "nohash", 60))
       return { tag="Splice", ast }
    else
       if parser_name == "expr" then ast = { { tag="Return", ast } }
       elseif parser_name == "stat"  then ast = { ast }
       elseif parser_name ~= "block" then
          error ("splice content must be an expr, stat or block") end
-      --printf("EXEC THIS SPLICE:\n%s", table.tostring(ast, "nohash", 60))
+      --printf("EXEC THIS SPLICE:\n%s", util.table_tostring(ast, "nohash", 60))
       return mlp.splice (ast)
    end
 end
