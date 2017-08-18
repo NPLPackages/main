719cfa73719d9769dced7c15373d314e92002b4a
lipengg <lipengg@163.com>
2017-08-18 16:21:58 +0800

    MCML2 测试文件

---
+++ script/test/MCMLV2/mcml_window.lua
@@ -0,0 +1,16 @@
+--[[
+Title: Code behind for mcml_window.html
+Author(s): LiXizhi
+Date: 2014/3/21
+Desc: demo code
+use the lib:
+------------------------------------------------------------
+NPL.load("(gl)script/test/MCMLV2/mcml_window.lua");
+local HelloWorld = commonlib.gettable("script.test.MCMLV2.mcml_window");
+-------------------------------------------------------
+]]
+local window = commonlib.gettable("script.test.MCMLV2.mcml_window");
+
+function window.OnInit()
+
+end
