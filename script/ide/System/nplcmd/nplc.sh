d5ef00588b8a343362a19df3c93e0f77ceb2eae3
Leopold Chen <placeintime.qh@gmail.com>
2018-01-15 17:20:57 +0800

    delete duplicate nplc bash file; add nplc bootstrapper

--- script/ide/System/nplcmd/nplc.sh
+++ script/ide/System/nplcmd/nplc.sh
@@ -9,10 +9,15 @@
 # nplc sum.npl 1 2 => load sum.npl and call the run function with params 1 2
 #---------------------------------------------------
 
+if [ $1 ] && ([ $1 == "-D" ] || [ $1 == "-d" ]); then
+    option = $1
+    shift
+fi
+
 function join { local IFS="$1"; shift; echo "$*"; }
 
 binder="+"
 
 options=$(join $binder $*)
 
-npl bootstrapper="(gl)script/ide/System/nplcmd/cmd.npl" i="true" servermode="true" nplcmd="$options" cmd_path="$PWD" # logfile="/var/log/nplc/log.txt"
+npl $option bootstrapper="(gl)script/ide/System/nplcmd/cmd.npl" i="true" servermode="true" nplcmd="$options" cmd_path="$PWD" logfile="/var/log/nplc/log.txt"
