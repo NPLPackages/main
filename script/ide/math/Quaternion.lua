8d00c326514867831761b8c3088c37b9a31b9f6c
LXZ_svn_sync <lixizhi@yeah.net>
2018-02-08 18:07:10 +0800

    svn_sync

--- script/ide/math/Quaternion.lua
+++ script/ide/math/Quaternion.lua
@@ -65,6 +65,16 @@ function Quaternion.__mul(a,b)
 	});
 end
 
+-- multiplay in place without creating a new quaternion
+function Quaternion:multiplyInplace(b)
+	local a = self;
+	self[1], self[2], self[3], self[4] = a[4] * b[1] + a[1] * b[4] + a[2] * b[3] - a[3] * b[2],
+		a[4] * b[2] + a[2] * b[4] + a[3] * b[1] - a[1] * b[3],
+		a[4] * b[3] + a[3] * b[4] + a[1] * b[2] - a[2] * b[1],
+		a[4] * b[4] - a[1] * b[1] - a[2] * b[2] - a[3] * b[3]
+	return self;
+end
+
 --This constructor creates a new quaternion that will rotate vector
 -- a into vector b about their mutually perpendicular axis. (if one exists)
 function Quaternion:FromVectorToVector(a, b)
