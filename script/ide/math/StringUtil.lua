d50b7811dc488432c44f77cf07c4dd93cd07637b
LXZ_svn_sync <lixizhi@yeah.net>
2019-09-25 18:44:36 +0800

    svn_sync

--- script/ide/math/StringUtil.lua
+++ script/ide/math/StringUtil.lua
@@ -11,27 +11,25 @@ local StringUtil = commonlib.gettable("mathlib.StringUtil");
 ]]
 local StringUtil = commonlib.gettable("mathlib.StringUtil");
 
+local byte = string.byte;
+
 --Given two non-empty strings as parameters, this method will return the length of the longest substring common to both parameters. 
 function StringUtil.LongestCommonSubstring(str1, str2)
 	if (not str1  or not str2) then
 		return 0;
 	end
 	local num = {};
-	for i = 1 , #str1 do
-		num[i] = {};
-	end
 
 	local maxlen = 0;
 	for i = 1 , #str1 do
+		num[i] = {};
 		for j = 1, #str2 do
-			if (str1[i] ~= str2[j]) then
-				num[i][j] = 0;
-			else
+			if (byte(str1, i) == byte(str2, j)) then
 				local count = 0;
 				if ((i == 1) or (j == 1)) then
 					count = 1;
 				else
-					count = 1 + num[i - 1][j - 1];
+					count = 1 + (num[i - 1][j - 1] or 0);
 				end
 				if (count > maxlen) then
 					maxlen = count;
