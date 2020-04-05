29a51dcb8c08482497746407b06b8e891cde35ae
LXZ_svn_sync <lixizhi@yeah.net>
2020-04-05 18:06:03 +0800

    svn_sync

--- script/ide/math/StringUtil.lua
+++ script/ide/math/StringUtil.lua
@@ -47,4 +47,72 @@ end
 
 function StringUtil.trim(str)
 	return str and str:gsub("^%s+",""):gsub("%s+$","");
-end;
+end
+
+-- @param code: text to search in
+-- @param text: text to search
+-- return true, filename, filenames: if the file text is found. filename contains the full filename
+-- if multiple results are found, return filenames which is array of filename
+function StringUtil.FindTextInLine(code, text, bExactMatch)
+	if(code) then
+		if(bExactMatch and code == text) then
+			return true, text;
+		elseif(not bExactMatch) then
+			local nFromIndex = 1;
+			local filename, filenames;
+			local lineNumber = 1;
+			local lastEnter;
+			while(nFromIndex) do
+				local from = code:find(text, nFromIndex)
+				if(from) then
+					local line = "";
+					if(from > 2) then
+						while(true) do
+							local nEnterPos = code:find("\n", (lastEnter or 0)+1)
+							if(nEnterPos) then
+								if(nEnterPos > from) then
+									break;
+								else
+									lastEnter = nEnterPos;
+									lineNumber = (lineNumber or 1) + 1;
+								end
+							else
+								break;
+							end
+						end
+						if(lastEnter) then
+							line = code:sub(lastEnter+1, from-1);
+						else
+							line = code:sub(math.max(1, from-50), from-1);
+						end
+					end
+					local lineText = code:sub(from, from + 100);
+					local lastEnter = lineText:find("\n")
+					if(lastEnter) then
+						lineText = lineText:sub(1, lastEnter-1)
+						nFromIndex = math.max(from + lastEnter + 1, nFromIndex + 1);
+					else
+						nFromIndex = code:find("\n", from+100+1);
+						if(nFromIndex) then
+							nFromIndex = nFromIndex + 1;
+						end
+					end
+					line = format("%d: %s%s", lineNumber or 1, line, lineText);
+					if(not filename) then
+						filename = line;
+					else
+						if(not filenames) then
+							filenames = {filename}
+						end
+						filenames[#filenames+1] = line
+					end
+				else
+					nFromIndex = nil;
+				end
+			end
+			if(filename) then
+				return true, filename, filenames
+			end
+		end
+	end
+end
