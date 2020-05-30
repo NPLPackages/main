a0edc3085c4eca90bf0ec793de70070c3e8f2038
onedou <onedou@126.com>
2020-05-30 16:38:52 +0800

    set field MoveViewWhenAttackWithIME in pe editor

--- script/kids/3DMapSystemApp/mcml/pe_editor.lua
+++ script/kids/3DMapSystemApp/mcml/pe_editor.lua
@@ -1397,6 +1397,17 @@ function pe_editor_text.create(rootName, mcmlNode, bindingContext, _parent, left
 			_this.background = css.background;
 			_parent:AddChild(_this);
 		end	
+
+		local screenLeft, screenTop, screenWidth, screenHeight = ParaUI.GetUIObject("root"):GetAbsPosition();
+		local cLeft, cTop, cWidth, cHeight = _this:GetAbsPosition();
+		local moveViewWhenAttackWithIME = false
+
+		if ((cTop + cHeight) > (screenHeight / 2)) then
+			moveViewWhenAttackWithIME = true
+		end
+
+		_this:SetField("MoveViewWhenAttackWithIME", moveViewWhenAttackWithIME);
+
 		if(_this and mcmlNode:GetBool("ReadOnly")) then
 			_this.enabled = false;
 		end
